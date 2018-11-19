974c387357d8f51a51a0546f2d3638798ca20438
tejchauhan <32064873+tejchauhan@users.noreply.github.com>
2018-11-19 09:30:18 +0530

    Update practical13-1.cpp

--- practical13-1.cpp
+++ practical13-1.cpp
@@ -5,23 +5,22 @@ int a[10][10],i,j,k,n,qu[10],front,rare,v,visit[10],visited[10],x,y;
 
 int main()
 {
-cout <<"enter no of vertices SIR/MAM!";
+cout <<"enter no of vertices";
 cin >> n;
-cout<<"sir please enter for undirected grah"<<endl;
-     cin>>x>>y;
-    if(x>=n&&y>=n)
-        cout<<"sir ji please enter within range"<<endl;
-        else
-        {
-    while(x!=-1&&y!=-1)
+cout<<"enter for undirected grah"<<endl;
+cin>>x>>y;
+if(x>=n&&y>=n)
+     cout<<"enter within range"<<endl;
+else
+{
+     while(x!=-1&&y!=-1)
     {
         a[x][y]=1;
         a[y][x]=1;
         cin>>x>>y;
 
     }
-    }
-
+}
 cout <<"enter initial vertex";
 cin >>v;
 cout <<"Visitied vertices\n";
